98c11334cf1c37a05f877053febbd4ef3fdbe86b
hirocueki <hirocueki@gmail.com>
2019-07-23 18:32:06 +0900

    tweak

--- app.rb
+++ app.rb
@@ -10,10 +10,14 @@ end
 
 get '/hello/*' do |name|
   svg = InitialAvatar.avatar_data_uri(name, size: 50, font_size: 20)
-  svg2 = InitialAvatar.avatar_data_uri(name[0], size: 50, font_size: 20)
+
+  sample_color = InitialAvatar::Configuration.new.colors.sample
+  svg2 = InitialAvatar.avatar_data_uri(name[0], size: 50, font_size: 20, color: sample_color)
+
   """
   <h1>hello #{name}</h1>
   <img src='#{svg}'/>
   <img src='#{svg2}'/>
+  #{sample_color}
   """
 end
